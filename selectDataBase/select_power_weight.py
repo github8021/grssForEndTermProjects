d6d9066b9ea024799b683388799e7d0e6a57337a
YimaiDuan <dym0822@163.com>
2020-12-02 20:57:58 +0800

    完成了权重表查询所有

---
+++ selectDataBase/select_power_weight.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Project  :grssForEndTermProjects
+# @File     :select_power_weight
+# @Date     :2020/12/2 8:55 下午
+# @Author   :段益迈
+# @Email    :dym0822@163.com
+# @Software :PyCharm
+-------------------------------------------------
+"""
+import pymysql
+
+db_username = 'root'
+db_password = 'root1234'
+db_name = 'grss'
+db = pymysql.connect("localhost", db_username, db_password, db_name)
+
+# cursor
+cursor = db.cursor()
+
+def select_power_weight():
+    cursor.execute("select * from power_weight")
+    power_weight_info = cursor.fetchall()
+    return power_weight_info
+
+
+# print(select_power_weight())
