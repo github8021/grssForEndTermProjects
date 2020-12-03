205262747749930c11cc886bb2cdf2560c272bf8
YimaiDuan <dym0822@163.com>
2020-12-03 13:16:15 +0800

    fix bugs 12.3

---
+++ OperateDatabase/insert_balance_horizontal.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+# @Project  :grssForEndTermProjects
+# @File     :insert_balance_horizontal
+# @Date     :2020/12/3 12:31 下午
+# @Author   :段益迈
+# @Email    :dym0822@163.com
+# @Software :PyCharm
+-------------------------------------------------
+"""
--- selectDataBase/select_balance_horizontal.py
+++ selectDataBase/select_balance_horizontal.py
@@ -30,7 +30,7 @@ db = pymysql.connect("localhost", db_username, db_password, db_name)
 # cursor
 cursor = db.cursor()
 
-def select_balance_horizontal(known_table,known_table_attribute,company_id):
+def select_balance_horizontal(known_table,known_table_attribute,company_id,company_year):
     tablelist=[]
     columnlist=[]
     alist = []
@@ -43,11 +43,11 @@ def select_balance_horizontal(known_table,known_table_attribute,company_id):
     # print(tablelist)
     # print(columnlist)
     for listnum in range(0, len(result)):
-        cursor.execute("select %s from %s where company_id=%d" %
-                       (columnlist[listnum], tablelist[listnum],company_id))
+        cursor.execute("select %s from %s where company_id=%d and company_year=%d" %
+                       (columnlist[listnum], tablelist[listnum],company_id,company_year))
         result1 = cursor.fetchall()
         for i in result1:
             alist.append(i[0])
-    print(alist)
     return alist
-select_balance_horizontal("'balance_horizontal'","'meaapor'",1)
+
+
